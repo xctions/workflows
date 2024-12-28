68db37acb5ddae8d3069220e7c8f29b4cf8c4a56
zdpk <zdpk@users.noreply.github.com>
2024-12-28 18:23:14 +0900

    .

--- test.sh
+++ test.sh
@@ -1,14 +1,28 @@
-a="abc/a"
-b="/abc/a"
+# a="abc/a"
+# b="/abc/a"
 
-if [[ "$a" == "/"* ]]; then
-    echo "a starts with /"
-else
-    echo "a not starts with /"
+# if [[ "$a" == "/"* ]]; then
+#     echo "a starts with /"
+# else
+#     echo "a not starts with /"
+# fi
+
+# if [[ "$b" == "/"* ]]; then
+#     echo "b starts with /"
+# else
+#     echo "b not starts with /"
+# fi
+
+
+###
+if [[ -e "a" ]]; then
+    echo "file exists"
 fi
 
-if [[ "$b" == "/"* ]]; then
-    echo "b starts with /"
-else
-    echo "b not starts with /"
+if [[ -e "b" ]]; then
+    echo "dir exists"
+fi
+
+if [[ ! -e "c" ]]; then
+    echo "dir c doesn't exist"
 fi
